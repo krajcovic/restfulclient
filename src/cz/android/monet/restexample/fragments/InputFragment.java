caa8fc09942834484964740c2add08eed73b4fa5
Dusan Krajcovic <dkrajcovic@monetplus.cz>
2012-05-04 13:50:02 +0200

    Intents - startActivity without result.

--- src/cz/android/monet/restexample/fragments/InputFragment.java
+++ src/cz/android/monet/restexample/fragments/InputFragment.java
@@ -1,9 +1,15 @@
 package cz.android.monet.restexample.fragments;
 
+import java.util.List;
+
 import cz.android.monet.restexample.R;
 import cz.android.monet.restexample.MyAsyncTask;
 import cz.android.monet.restexample.interfaces.OnServerResultReturned;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -18,10 +24,7 @@ public class InputFragment extends Fragment {
 
 	private EditText host;
 	private EditText sendData;
-	private String receiveData;
-	
 	OnServerResultReturned mResultCallback;
-
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -46,6 +49,43 @@ public class InputFragment extends Fragment {
 						.getText().toString(), mResultCallback);
 			}
 
+		});
+
+		Button butTest = (Button) getView().findViewById(R.id.btnReadBarCode);
+		butTest.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Uri number = Uri.parse("tel:734423665");
+				Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+
+				// Map point based on address
+				Uri location = Uri
+						.parse("geo:0,0?q=1600+Amphitheatre+Parkway,+Mountain+View,+California");
+				// Or map point based on latitude/longitude
+				// Uri location = Uri.parse("geo:37.422219,-122.08364?z=14"); //
+				// z param is zoom level
+				Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+
+				Uri webpage = Uri.parse("http://www.android.com");
+				Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+				
+				Intent intent = new Intent(Intent.ACTION_SEND);
+
+				// Always use string resources for UI text. This says something like "Share this photo with"
+				String title = "Vyberte aplikaci";
+				// Create and start the chooser
+				Intent chooser = Intent.createChooser(intent, title);
+	
+				PackageManager packageManager = getActivity().getApplicationContext().getPackageManager();
+				List<ResolveInfo> activities = packageManager
+						.queryIntentActivities(webIntent, 0);
+				boolean isIntentSafe = activities.size() > 0;
+
+				if (isIntentSafe) {
+					startActivity(chooser);
+				}
+			}
 		});
 	}
 
