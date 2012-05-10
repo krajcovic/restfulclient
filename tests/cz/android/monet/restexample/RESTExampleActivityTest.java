4ce0e9d19cb850ea2224e03c760e50bb22d83c12
Dusan Krajcovic <dkrajcovic@monetplus.cz>
2012-05-10 10:37:49 +0200

    IN PROGRESS - issue ANDROIDAUTH-3: Add unit testing to project http://jira.monetplus.cz/browse/ANDROIDAUTH-3
    Generated first JUnit test case. (The generate state for a rollback)

---
+++ tests/cz/android/monet/restexample/RESTExampleActivityTest.java
@@ -0,0 +1,16 @@
+package cz.android.monet.restexample;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class RESTExampleActivityTest extends
+		ActivityInstrumentationTestCase2<RESTExampleActivity> {
+
+	public RESTExampleActivityTest(Class<RESTExampleActivity> name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
