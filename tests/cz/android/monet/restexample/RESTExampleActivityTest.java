682f220bf8d0f545a460dafe9a3db44f9bf13e51
Dusan Krajcovic <dkrajcovic@monetplus.cz>
2012-05-10 13:32:00 +0200

    getActivity in test.

--- tests/cz/android/monet/restexample/RESTExampleActivityTest.java
+++ tests/cz/android/monet/restexample/RESTExampleActivityTest.java
@@ -5,12 +5,14 @@ import android.test.ActivityInstrumentationTestCase2;
 public class RESTExampleActivityTest extends
 		ActivityInstrumentationTestCase2<RESTExampleActivity> {
 
-	public RESTExampleActivityTest(Class<RESTExampleActivity> name) {
-		super(name);
+	public RESTExampleActivityTest() {
+		super("cz.android.monet.restexample", RESTExampleActivity.class);  
 	}
 
 	protected void setUp() throws Exception {
 		super.setUp();
+		
+		RESTExampleActivity testedActivity = getActivity();  
 	}
 
 }
