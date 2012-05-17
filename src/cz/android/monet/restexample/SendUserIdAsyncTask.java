f89e0e99873162f8d20c3d08a6e570030ce01074
Dusan Krajcovic <dkrajcovic@monetplus.cz>
2012-05-17 14:36:52 +0200

    RESOLVED - issue ANDROIDAUTH-20: IllegalArgument in monkey test. http://jira.monetplus.cz/browse/ANDROIDAUTH-20 Expanded exception to sendData.

--- src/cz/android/monet/restexample/SendUserIdAsyncTask.java
+++ src/cz/android/monet/restexample/SendUserIdAsyncTask.java
@@ -1,12 +1,14 @@
 package cz.android.monet.restexample;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
@@ -43,28 +45,28 @@ public class SendUserIdAsyncTask extends AsyncTask<Object, Void, String> {
 
 	protected String sendData(String targetDomain, String userId) {
 
-		if (!targetDomain.matches("^\\s*(.*?)")) {
-			Log.e(TAG, "Invalid host string | " + targetDomain);
-			return null;
-		}
-
-		String urlToSendRequest = "http://" + targetDomain + ":" + "2323"
-				+ "/restfulexample/app/user/" + Integer.parseInt(userId);
-
-		DefaultHttpClient httpClient = new DefaultHttpClient();
-
-		HttpHost targetHost = new HttpHost(targetDomain, 2323, "http");
-		// Using GET here
-		HttpGet httpGet = new HttpGet(urlToSendRequest);
-
-		// Make sure the server knows what kind of a response we will accept
-		httpGet.addHeader("Accept", "application/xml");
-
-		// Also be sure to tell the server what kind of content we are
-		// sending
-		httpGet.addHeader("Content-Type", "application/xml");
-
 		try {
+			if (!targetDomain.matches("^\\s*(.*?)")) {
+				Log.e(TAG, "Invalid host string | " + targetDomain);
+				return null;
+			}
+
+			String urlToSendRequest = "http://" + targetDomain + ":" + "2323"
+					+ "/restfulexample/app/user/" + Integer.parseInt(userId);
+
+			DefaultHttpClient httpClient = new DefaultHttpClient();
+
+			HttpHost targetHost = new HttpHost(targetDomain, 2323, "http");
+			// Using GET here
+			HttpGet httpGet = new HttpGet(urlToSendRequest);
+
+			// Make sure the server knows what kind of a response we will accept
+			httpGet.addHeader("Accept", "application/xml");
+
+			// Also be sure to tell the server what kind of content we are
+			// sending
+			httpGet.addHeader("Content-Type", "application/xml");
+
 			// execute is a blocking call, it's best to call this code in a
 			// thread separate from the ui's
 			HttpResponse response = httpClient.execute(targetHost, httpGet);
@@ -75,10 +77,14 @@ public class SendUserIdAsyncTask extends AsyncTask<Object, Void, String> {
 			response.getEntity().writeTo(outstrem);
 			return outstrem.toString();
 
-		} catch (Exception ex) {
+		} catch (ClientProtocolException ex) {
+			ex.printStackTrace();
+			Log.e(TAG, ex.getMessage());
+		} catch (IOException ex) {
 			ex.printStackTrace();
 			Log.e(TAG, ex.getMessage());
 		}
+
 		return null;
 	}
 }
